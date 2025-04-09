beb9781fd1b4dc3f0caefcfe83d296a79671ac95
Chris Millar <cmillar@adobe.com>
2025-04-09 05:39:42 -0600

    Fix footer issue

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -3,7 +3,7 @@ import { getConfig, getMetadata, loadArea } from '../../scripts/nx.js';
 async function getNavEl() {
   const source = getMetadata('footer-source') || '/fragments/nav/footer';
   const resp = await fetch(`${source}.plain.html`);
-  if (!resp.ok) return;
+  if (!resp.ok) return null;
   const html = await resp.text();
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, 'text/html');
@@ -25,6 +25,7 @@ function decorateNav(nav) {
 export default async function init(el) {
   const { decorateArea } = getConfig();
   const nav = await getNavEl();
+  if (!nav) return;
   el.append(nav);
   await decorateArea(nav);
   await loadArea(nav);
